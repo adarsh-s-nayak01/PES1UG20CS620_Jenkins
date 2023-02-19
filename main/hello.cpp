1a416d7d701aa9feefd7a5954469f7b5ff557694
Adarsh Nayak <89061544+adarsh-s-nayak01@users.noreply.github.com>
2023-02-19 23:04:41 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I'm Adarsh and I have successfully built and run\n";
+  cout << "I'm Adarsh , my SRN is PES1UG20CS620 and I have successfully built and run\n";
   return 0;
 }
